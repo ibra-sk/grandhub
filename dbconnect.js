4e3c3342bd887ca58dfaea337795315a9ee6d7e1
Ibrahim Saleh <76790442+ibra-sk@users.noreply.github.com>
2022-03-31 23:18:14 +0300

    fix DB reconnect Issue

--- dbconnect.js
+++ dbconnect.js
@@ -1,21 +1,53 @@
 const mysql = require('mysql');
 let instance = null;
+let dbconnection;
 
-const connection = mysql.createConnection({
+let db_config = {
+    // host: "localhost",
+    // user: "root",
+    // password: "",
+    // database: 'delivrbase',
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b2a5fa40654afb',
     password: '556171fa',
     database: 'heroku_6003e04e89b9d0d',
-    port: '3306'
-});
+    connectionLimit: 50,
+    queueLimit: 0,
+    waitForConnection: true,
+    debug: false,
+    timezone: 'utc',
+    supportBigNumbers: true,
+    bigNumberStrings: true
+};
 
-connection.connect( (err) => {
-    if(err){
-        //console.log(err.message);
-        console.log('db refused connection');
-    }
-    console.log(`db ${connection.state}`);
-})
+let tempPool = mysql.createPool(db_config);
+    tempPool.getConnection(function(err, connection) {
+        
+        dbconnection = connection;
+        if(err){
+            console.log('db refused connection');
+            console.log(err);
+        }
+        console.log(`db ${connection.state}`);
+    });
+
+console.log("Waiting for stuff....");
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'delivrbase',
+//     port: '3306'
+// });
+
+// connection.connect( (err) => {
+//     if(err){
+//         //console.log(err.message);
+//         console.log('db refused connection');
+//     }
+//     console.log(`db ${connection.state}`);
+// })
 
 class DbService {
     static getDbServiceInstance() {
@@ -26,10 +58,34 @@ class DbService {
         try {
             const response = await new Promise((resolve, reject) => {
                 const query = q;
-                connection.query(query, params, (err, results) => {
-                    if(err) reject(new Error(err.message));
-                    resolve(results);
-                })
+                
+                //console.log(dbconnection.tempPool.state)
+                console.log(dbconnection.state)
+
+                if(dbconnection.state == "disconnected"){
+                    tempPool.getConnection(function(err, connection) {
+        
+                        dbconnection = connection;
+                        if(err){
+                            console.log('db refused connection');
+                            console.log(err);
+                        }else{
+                            dbconnection.query(query, params, (err, results) => {
+                                if(err) reject(new Error(err.message));
+                                resolve(results);
+                            })
+                        }
+                        console.log(`db ${connection.state}`);
+                    });
+                    
+                }else{
+                    dbconnection.query(query, params, (err, results) => {
+                        if(err) reject(new Error(err.message));
+                        resolve(results);
+                    })
+                }
+                
+                
             });
 
             //console.log(response);
